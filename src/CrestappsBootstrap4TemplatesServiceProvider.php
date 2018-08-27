caa431149c8e595b40860dd9146a20f1d6533f26
Ybansan <42271181+Ybansan@users.noreply.github.com>
2018-08-27 17:05:36 +0200

    bug

--- src/CrestappsBootstrap4TemplatesServiceProvider.php
+++ src/CrestappsBootstrap4TemplatesServiceProvider.php
@@ -5,7 +5,7 @@ namespace Ybansan\CrestappsBootstrap4Templates;
 use File;
 use Illuminate\Support\ServiceProvider;
 
-class CrestappsB4TemplatesServiceProvider extends ServiceProvider
+class CrestappsBootstrap4Templates extends ServiceProvider
 {
     /**
      * Perform post-registration booting of services.
